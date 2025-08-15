11a24243a69f2547f9797c4cdc2338a5d4ecce53
PhilipGeier <philipgeieru@gmail.com>
2025-08-15 14:09:58 +0200

    Custom stuff should work now

--- renlib.py
+++ renlib.py
@@ -1,18 +1,27 @@
 ﻿import io
-import pathlib
 
 import decompiler
-from unrpyc import Context, get_ast
+import deobfuscate
+from unrpyc import Context, read_ast_from_file
 
 
-def renlib_decompile_rpyc(filepath: str, try_harder=False):
-    file = pathlib.Path(filepath)
+def renlib_get_ast(file_bytes: io.BytesIO, try_harder=False):
+    if try_harder:
+        ast = deobfuscate.read_ast(file_bytes, Context())
+    else:
+        ast = read_ast_from_file(file_bytes, Context())
+    return ast
 
-    ast = get_ast(file, try_harder, Context())
+def renlib_decompile_rpyc(file_bytes: bytes, try_harder=False):
+    bytes_io = io.BytesIO()
+    bytes_io.write(file_bytes)
+    bytes_io.seek(0)
 
-    strOutput = io.StringIO()
+    ast = renlib_get_ast(bytes_io, try_harder)
+
+    str_output = io.StringIO()
 
     options = decompiler.Options(log=lambda: print(""), translator=None, init_offset=None, sl_custom_names=None)
-    decompiler.pprint(strOutput, ast, options)
+    decompiler.pprint(str_output, ast, options)
 
-    return strOutput.getvalue()
+    return str_output.getvalue()
