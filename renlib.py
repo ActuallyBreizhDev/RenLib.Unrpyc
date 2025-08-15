5ae906964c3d433de8e832e6744cab0291fd0e0f
PhilipGeier <philipgeieru@gmail.com>
2025-08-15 11:31:38 +0200

    Added custom functions required by RenLib

---
+++ renlib.py
@@ -0,0 +1,18 @@
+﻿import io
+import pathlib
+
+import decompiler
+from unrpyc import Context, get_ast
+
+
+def renlib_decompile_rpyc(filepath: str, try_harder=False):
+    file = pathlib.Path(filepath)
+
+    ast = get_ast(file, try_harder, Context())
+
+    strOutput = io.StringIO()
+
+    options = decompiler.Options(log=lambda: print(""), translator=None, init_offset=None, sl_custom_names=None)
+    decompiler.pprint(strOutput, ast, options)
+
+    return strOutput.getvalue()
